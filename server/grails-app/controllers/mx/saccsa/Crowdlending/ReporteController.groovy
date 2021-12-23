50fe8978b8d769dcbd4802bb56a7c6f1cefd10ac
AlberttoG2 <albertt0801.ag@gmail.com>
2021-12-23 08:31:06 -0600

    Carta confirmacion

--- server/grails-app/controllers/mx/saccsa/Crowdlending/ReporteController.groovy
+++ server/grails-app/controllers/mx/saccsa/Crowdlending/ReporteController.groovy
@@ -1,18 +1,21 @@
 package mx.saccsa.Crowdlending
 
+import java.text.SimpleDateFormat
+
 class ReporteController {
     static responseFormats = ['json', 'xml']
     ReporteService reporteService
     def generateReportService
+    def sdf = new SimpleDateFormat('yyyy-MM-dd')
 
     def pruebaRep(){
-        def fechaInicio = (!params?.fechaInicio)?session.fecha:(sdf.parse(params?.fechaInicio))
-        def fechaFin = (!params?.fechaFin)?session.fecha:(sdf.parse(params?.fechaFin))
-        def lista = reporteService.pruebaRep(fechaInicio, fechaFin)
-        params._file = ''
+//        def fechaInicio = (!params?.fechaInicio)?null:(sdf.parse(params?.fechaInicio))
+//        def fechaFin = (!params?.fechaFin)?null:(sdf.parse(params?.fechaFin))
+        def lista = reporteService.pruebaRep(params.id)
+        params._file = 'cartaConfirmacion'
         params._format = 'PDF'
-        params._name = ''
-        params._reporteTitulo = ''
-        generateReportService.createReport(request,response,params, [data: [lista]])
+        params._name = 'Carta Confirmación'
+        params._reporteTitulo = 'Carta Confirmación'
+        generateReportService.createReport(request,response,params, [data: lista])
     }
 }
--- server/grails-app/services/mx/saccsa/Crowdlending/ReporteService.groovy
+++ server/grails-app/services/mx/saccsa/Crowdlending/ReporteService.groovy
@@ -10,34 +10,62 @@ class ReporteService {
     def sdf = new SimpleDateFormat('yyyy-MM-dd')
 
 
-    def pruebaRep(Date fechaInicio, Date fechaFin){
-        PruebaReporte pruebaReporte = PruebaReporte.findAllByFecha()
+//    def pruebaRep(Date fechaInicio, Date fechaFin){
+    def pruebaRep(Long id){
+//        def pruebaReporte = PruebaReporte.findByFechaBetween(fechaInicio, fechaFin).collect({
+        def pruebaReporte = PruebaReporte.findById(id).collect({
+            [
+                    valorDeCuenta: it.valorDeCuenta,
+                    interesResividos: it.interesResividos,
+                    intereseMoratorios: it.intereseMoratorios,
+                    iVADelInteresMoratorio: it.iVADelInteresMoratorio,
+                    comicionPorGestionCuenta: it.comicionPorGestionCuenta,
+                    comision: it.comision,
+                    ivaComision: it.ivaComision,
+                    fondosAgregados: it.fondosAgregados,
+                    retiros: it.retiros,
+                    valorDeCuentaAlFinalDelMes: it.valorDeCuentaAlFinalDelMes,
+                    dIsponibleAlFinalPeriodo: it.dIsponibleAlFinalPeriodo,
+                    ganadoALFinalDelPerido: it.ganadoALFinalDelPerido,
+                    recursosFondeados: it.recursosFondeados,
+                    pagosResividos: it.pagosResividos,
+                    prestamosRealizados: it.prestamosRealizados,
+                    comisionesPagadas: it.comisionesPagadas,
+                    recursosRetirados: it.recursosRetirados,
+                    alCorriente: it.alCorriente,
+                    entransito: it.entransito,
+                    atrasado: it.atrasado,
+                    vencido: it.vencido,
+                    pagado: it.pagado,
+                    fecha: it.fecha,
+            ]
+        })
 
-        def datos = [
-                valorDeCuenta: pruebaReporte.valorDeCuenta,
-                interesResividos: pruebaReporte.interesResividos,
-                intereseMoratorios: pruebaReporte.intereseMoratorios,
-                iVADelInteresMoratorio: pruebaReporte.iVADelInteresMoratorio,
-                comicionPorGestionCuenta: pruebaReporte.comicionPorGestionCuenta,
-                comision: pruebaReporte.comision,
-                ivaComision: pruebaReporte.ivaComision,
-                fondosAgregados: pruebaReporte.fondosAgregados,
-                retiros: pruebaReporte.retiros,
-                valorDeCuentaAlFinalDelMes: pruebaReporte.valorDeCuentaAlFinalDelMes,
-                dIsponibleAlFinalPeriodo: pruebaReporte.dIsponibleAlFinalPeriodo,
-                ganadoALFinalDelPerido: pruebaReporte.ganadoALFinalDelPerido,
-                recursosFondeados: pruebaReporte.recursosFondeados,
-                pagosResividos: pruebaReporte.pagosResividos,
-                prestamosRealizados: pruebaReporte.prestamosRealizados,
-                comisionesPagadas: pruebaReporte.comisionesPagadas,
-                recursosRetirados: pruebaReporte.recursosRetirados,
-                alCorriente: pruebaReporte.alCorriente,
-                entransito: pruebaReporte.entransito,
-                atrasado: pruebaReporte.atrasado,
-                vencido: pruebaReporte.vencido,
-                pagado: pruebaReporte.pagado,
-                fecha: pruebaReporte.fecha,
-        ]
-        return datos
+//        def datos = [
+//                valorDeCuenta: pruebaReporte.valorDeCuenta,
+//                interesResividos: pruebaReporte.interesResividos,
+//                intereseMoratorios: pruebaReporte.intereseMoratorios,
+//                iVADelInteresMoratorio: pruebaReporte.iVADelInteresMoratorio,
+//                comicionPorGestionCuenta: pruebaReporte.comicionPorGestionCuenta,
+//                comision: pruebaReporte.comision,
+//                ivaComision: pruebaReporte.ivaComision,
+//                fondosAgregados: pruebaReporte.fondosAgregados,
+//                retiros: pruebaReporte.retiros,
+//                valorDeCuentaAlFinalDelMes: pruebaReporte.valorDeCuentaAlFinalDelMes,
+//                dIsponibleAlFinalPeriodo: pruebaReporte.dIsponibleAlFinalPeriodo,
+//                ganadoALFinalDelPerido: pruebaReporte.ganadoALFinalDelPerido,
+//                recursosFondeados: pruebaReporte.recursosFondeados,
+//                pagosResividos: pruebaReporte.pagosResividos,
+//                prestamosRealizados: pruebaReporte.prestamosRealizados,
+//                comisionesPagadas: pruebaReporte.comisionesPagadas,
+//                recursosRetirados: pruebaReporte.recursosRetirados,
+//                alCorriente: pruebaReporte.alCorriente,
+//                entransito: pruebaReporte.entransito,
+//                atrasado: pruebaReporte.atrasado,
+//                vencido: pruebaReporte.vencido,
+//                pagado: pruebaReporte.pagado,
+//                fecha: pruebaReporte.fecha,
+//        ]
+        return pruebaReporte
     }
 }
